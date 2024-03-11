24ca668570d038a04778e14bb540fdfe9ffb40cc
charl <nicholas.shinder@gmail.com>
2024-03-11 17:40:29 +0000

    Db dep inj

---
+++ Database/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Database
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RecipeContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            return services;
+
+        }
+
+    }
+}
---
+++ Database/RecipeContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Source> Sources { get; set; }
+        public DbSet<SourceType> SourceTypes { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+    }
+}
